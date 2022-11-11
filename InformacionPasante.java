3a682ee3b27259cca22a69ad776606f0c0edafd7
antezana-cecilia <115126103+antezana-cecilia@users.noreply.github.com>
2022-11-11 10:20:42 -0400

    2.2 Ex

---
+++ InformacionPasante.java
@@ -0,0 +1,27 @@
+
+
+public class InformacionPasante {
+
+	public static void main (String [] args) {
+
+        int StudentAge = 36;
+        double Prom = 8.6 ;
+        String Midlename = "Rocio";
+        char Firstinitial = 'C';
+        char lastinitial = 'A';
+        boolean Attendance = true;
+
+
+
+	System.out.println ("\nLa edad del estudiante es: " + StudentAge +
+                        "\nEl promedio es: " + Prom +
+                        "\nEl nombre es: " + Midlename +
+                        "\nLa primera inicial es: " + Firstinitial + 
+                        "\nLa ultima inicial es: " + lastinitial +
+                        "\nLa participacion es: " + Attendance);
+	
+	}
+	
+
+
+}
